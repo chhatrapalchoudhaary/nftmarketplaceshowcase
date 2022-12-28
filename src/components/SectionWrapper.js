4216732e370a94659399c453bbb31d0d0c0f5725
chhatrapalchoudhaary <chhatrapalchoudhary101@gmail.com>
2022-12-28 09:09:02 +0530

    update app url

--- src/components/SectionWrapper.js
+++ src/components/SectionWrapper.js
@@ -44,7 +44,7 @@ const SectionWrapper = ({
           {showBtn && (
             <Button
               assetUrl={assets.expo}
-              link="https://expo.dev/@adrianhajdin/react_native_app?serviceType=classic&distribution=expo-go"
+              link="https://expo.dev/@cpchoudhary/nftmarketplace?serviceType=classic&distribution=expo-go"
             />
           )}
         </div>
